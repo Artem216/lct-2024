c3c6de50f9ab77dca028cc34ef5b8b00695e3f07
anyhhope <hopeanysimova@yandex.ru>
2024-06-08 13:45:42 +0300

    add middleware

--- backend/api-service/app/main.py
+++ backend/api-service/app/main.py
@@ -4,6 +4,7 @@ from api.endpoints import predict
 from api.endpoints import find
 from contextlib import asynccontextmanager
 from db.database import db
+from fastapi.middleware.cors import CORSMiddleware
 
 import os
 
@@ -30,6 +31,14 @@ async def lifespan(app: FastAPI):
     await pool.close()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to the appropriate origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Set this to the allowed HTTP methods
+    allow_headers=["*"],  # Set this to the allowed headers
+)
 
 app.include_router(auth.router)
 app.include_router(predict.router, prefix="/api/v1")
