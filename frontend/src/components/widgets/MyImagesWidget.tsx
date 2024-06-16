a7d7cb888c135d2c84fe5b1c98b58b408ed92625
anyhhope <hopeanysimova@yandex.ru>
2024-06-16 17:58:09 +0300

    add download images

--- frontend/src/components/widgets/MyImagesWidget.tsx
+++ frontend/src/components/widgets/MyImagesWidget.tsx
@@ -2,17 +2,40 @@ import { useAllImages } from "@/context/AllImagesContext"
 import ImageCard from "../shared/ImageCard"
 import ImageCarousel from "./ImageCarousel";
 import { useState } from "react";
-
+import { Button } from "../ui/button";
+import axios from "axios";
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 const MyImagesWidget = () => {
   const { myCards } = useAllImages();
   const [openDialog, setOpenDialog] = useState(false);
+
+  const downloadImages = async () => {
+    const zip = new JSZip();
+
+    // Загрузите каждое изображение и добавьте его в zip-архив
+    const imagePromises = myCards.map(async (card, index) => {
+      const response = await axios.get(card.parent_s3_url, { responseType: 'blob' });
+      zip.file(`Изображение${index + 1}.jpg`, response.data);
+    });
+
+    // Дождитесь загрузки всех изображений
+    await Promise.all(imagePromises);
+
+    // Создайте zip-архив и загрузите его
+    const content = await zip.generateAsync({ type: 'blob' });
+    saveAs(content, 'Сгенерированные_изображения.zip');
+  };
 
   return (
     <>
       <div className="w-[95%] mx-auto p-10 bg-light-1 rounded-2xl my-10">
         <div className="flex justify-between m-10 w-[80%] mx-auto items-center">
           <p className="h3-regular md:h3-bold text-left m-5 text-dark-1">МОИ ИЗОБРАЖЕНИЯ</p>
+          <Button className="shad-button_primary px-5 w-[200px]" onClick={downloadImages}>
+              Скачать изображения
+            </Button>
         </div>
         <div className="flex justify-between mx-20 flex-wrap gap-10">
           {myCards.map((card) => {
